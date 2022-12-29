0571c10deba8579c7ac75bc0cdb688ed49dde2db
Puja Bhamare <poojabhamare444@gmail.com>
2022-12-29 14:42:16 +0530

    Api intergration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "react-bootstrap";
-import Data from "./data.json";
 import { SectionDetails } from "./components/SectionDetails/SectionDetails";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Loader } from "./components/Loader/Loader";
+
 function App() {
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadDetails();
+  }, []);
+
   const loadDetails = () => {
-    console.log(Data.results);
+    setLoading(true);
+    axios({
+      method: "get",
+      url: "https://randomuser.me/api",
+    })
+      .then((items) => {
+        setUserData(items.data.results[0]);
+      })
+      .catch((error) => {
+        console.log("Error while loading user details:", error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
   return (
     <div className="user-generator-page">
       <div className="header-section">
-        <h4>Krishna Bhamare</h4>
+        <h4>
+          {userData?.name?.first} {userData?.name?.last}
+        </h4>
         <Button variant="dark" onClick={loadDetails}>
           Generate New User
         </Button>
       </div>
-      <SectionDetails detail={Data.results[0]} />
+      {loading ? <Loader /> : <SectionDetails detail={userData} />}
     </div>
   );
 }
---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Loader = ()=> {
+  return (
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+        Loading the details
+    </div>
+  )
+}
